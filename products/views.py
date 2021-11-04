7590b927dc23ae68ef07674baa307eaa498b7516
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-04 15:57:30 +0300

    update serializers

--- products/views.py
+++ products/views.py
@@ -11,3 +11,22 @@
 #         products = Product.objects.all()[0:4]
 #         serializer = ProductSerializer(products, many=True)
 #         return Response(serializer.data)
+# class ProductDetail(APIView):
+#     def get_object(self, category_slug, product_slug):
+#         try:
+#             return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+#         except Product.DoesNotExist:
+#             raise Http404
+
+#     def get(self,request,category_slug, product_slug, format=None):
+#         product = self.get_object(category_slug,product_slug)
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)  
+
+
+# class CategoryDetail(APIView):
+#     def_object(self,category_slug):
+#       try:
+#           return Category.objects.get(slug=category_slug)
+#       except Category.DoesNotExist:
+#           raise Http404                      
