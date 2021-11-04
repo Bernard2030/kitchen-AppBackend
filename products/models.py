358183f4a982b6aecaa6ac9b3d77ac167cb2d7c0
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-04 17:32:50 +0300

    add urls for products

--- products/models.py
+++ products/models.py
@@ -1,29 +1,40 @@
-# from django.db import models
-# from cloudinary.models import CloudinaryField
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.core.files import File
 
-# # Create your models here.
+# Create your models here.
 
-# class Category(models.Model):
-#     name = models.CharField(max_length=200)
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField()
     
-#     class Meta:
-#         ordering = ('name')
+    class Meta:
+        ordering = ('name')
 
-#     def __str__(self):
-#         return self.name   
+    def __str__(self):
+        return self.name  
 
-
-# class Product(models.Model):
-#     category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
-#     name = models.CharField(max_length=255)
-#     description = models.TextField(blank=True,null=True)
-#     price = models.DecimalField(max_digits=6, decimal_places=2)
-#     image = CloudinaryField('image') 
-#     date_added = models.DateTimeField(auto_now_add=True)
+    def get_absolute_url(self):
+        return f'/{self.slug}'     
 
 
-#     class Meta:
-#         ordering = ('-date_added',)
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True,null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = CloudinaryField('image') 
+    date_added = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.name           
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name   
+
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+
+        return ''                
--- products/serializers.py
+++ products/serializers.py
@@ -1,14 +1,14 @@
-# from django.db.models import fields
-# from rest_framework import serializers
-# from .models import Category,Product
+from rest_framework import serializers
+from .models import Category,Product
 
-# class ProductSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model =Product
-#         fields = ('id','name','description','price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Product
+        fields = ('id','name','get_absolute_url','description','price','get_image')
 
 
-# class CategorySerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ('id','name','products')        
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = ('id','name','get_absolute_url','products')        
