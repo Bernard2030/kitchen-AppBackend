dacd1e519f999f43a83b595d0d93b71874ec07af
Bernard Opiyo <brobernard.254@gmail.com>
2021-11-04 09:07:57 +0300

    add login

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
 
 # create your views here
 class RegisterView(APIView):
@@ -12,4 +14,22 @@ class RegisterView(APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+
+class LoginView(APIView):
+    def post(self,request):
+        email = request.data('email')
+        password = request.data('password')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('user not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('incorrect password')  
+
+        return Response(user)      
+
+
         
